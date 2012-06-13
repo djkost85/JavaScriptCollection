405b6c5a8b4735ae17228ce129d56738ebfb988a
Mason Wan <masonaxcte@gmail.com>
2012-06-13 02:00:30 -0700

    Convert spaces to indents.

--- Download Search/Download Search.js
+++ Download Search/Download Search.js
@@ -1,17 +1,18 @@
 (function() {
-  var links = [
-    "http://beta.demonoid.me/files/?sort=S&query=%s", "http://www.kat.ph/usearch/%s/",
-    "https://www.google.com/cse?cx=014395573825771924245:6cxgcwinhte&q#gsc.tab&gsc.q=%s",
-    "https://www.google.com/search?q=mediafire|rapidshare|4shared|fileserve|hotfile|filesonic|depositfiles|zshare %s"
-  ];
+	var links = [
+		"http://beta.demonoid.me/files/?sort=S&query=%s",
+		"http://www.kat.ph/usearch/%s/",
+		"https://www.google.com/cse?cx=014395573825771924245:6cxgcwinhte&q#gsc.tab&gsc.q=%s",
+		"https://www.google.com/search?q=mediafire|rapidshare|4shared|fileserve|hotfile|filesonic|depositfiles|zshare %s"
+	];
 
-  var query = window.getSelection().toString().trim();
-  query = prompt('Download Search', query);
+	var query = window.getSelection().toString().trim();
+	query = prompt('Download Search', query);
 
-  if ((query != null) && (query = encodeURIComponent(query.trim()))) {
-    for (i = 0, len = links.length; i < len; i++) {
-      link = links[i];
-      window.open(link.replace('%s', query));
-    }
-  }
+	if ((query != null) && (query = encodeURIComponent(query.trim()))) {
+		for (i = 0, len = links.length; i < len; i++) {
+			link = links[i];
+			window.open(link.replace('%s', query));
+		}
+	}
 }).call(this);
