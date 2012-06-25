663c3d6a3774774d9bd0c5d9c76abdd7f06d630c
Mason Wan <masonaxcte@gmail.com>
2012-06-25 05:22:13 -0700

    Update version number and refine indents.

--- PandoraAutoContinue/PandoraAutoContinue UserScripts.user.js
+++ PandoraAutoContinue/PandoraAutoContinue UserScripts.user.js
@@ -1,11 +1,11 @@
 // ==UserScript== http://wiki.greasespot.net/Metadata_block
 // @name            Pandora Auto Continue
-// @namespace		http://userscripts.org/users/masonaxcte
+// @namespace       http://userscripts.org/users/masonaxcte
 // @description     Automatically click "I'm listening" and reload button in Pandora.com
 // @match           http://www.pandora.com/*
-// @run-at			document-end
+// @run-at          document-end
 // @updateURL       https://github.com/masonaxcte/JavaScriptCollection/raw/master/PandoraAutoContinue/PandoraAutoContinue%20UserScripts.user.js
-// @version         1.0
+// @version         1.1
 // ==/UserScript==
 
 console.log('Pandora Auto Continue');
