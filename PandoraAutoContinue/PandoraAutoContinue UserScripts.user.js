bf63e8cbc594d4266126f79a8be71bcbb6d084f2
Mason Wan <masonaxcte@gmail.com>
2012-06-25 05:02:50 -0700

    Added: auto-click reload button. Added: logging.

--- PandoraAutoContinue/PandoraAutoContinue UserScripts.user.js
+++ PandoraAutoContinue/PandoraAutoContinue UserScripts.user.js
@@ -8,17 +8,66 @@
 // @version         1.0
 // ==/UserScript==
 
+console.log('Pandora Auto Continue');
+
+var logs = [];
+loadLogs();
+
+function addLogs(log) {
+	logs.push(log);
+	saveLogs();
+}
+
+function saveLogs() {
+	var obj = {};
+
+	try {
+		obj = JSON.stringify(logs);
+	} catch (e) {
+		console.error('Error on stringifying logs');
+	}
+
+	localStorage.setItem('logs', obj);
+
+	console.log(obj.replace(/{/g, '\n\t{'));
+}
+
+function loadLogs() {
+	var obj = localStorage.getItem('logs');
+
+	if (obj !== null) {
+		try {
+			logs = JSON.parse(obj);
+		} catch (e) { }
+	}
+}
+
+addLogs({time: new Date(), type: 'start'});
+var timeStarted = new Date();
+
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function (mutations) {
 	mutations.forEach(function (mutation) {
 		for (var i = 0; i < mutation.addedNodes.length; i++) {
-			var addedNode = mutation.addedNodes[i];			
-			var node = addedNode.querySelector('a.still_listening')||	// still listening button
+			var addedNode = mutation.addedNodes[i];
+			var node = addedNode.querySelector('a.still_listening') ||	// still listening button
 				addedNode.querySelector('a.toastItemReload');	// reload button
 
 			if (node) {
 				node.click();
+
+				var elapsedSeconds = ((new Date()) - timeStarted) / 1000;
+
+				if (node.className.contains('still_listening')) {
+					console.log('listening');
+					addLogs({ time: new Date(), type: 'listening', elapsedSeconds: elapsedSeconds });
+				} else {
+					console.log('reload');
+					addLogs({ time: new Date(), type: 'reload', elapsedSeconds: elapsedSeconds });
+				}
+
+				return;
 			}
 		}
 	});
