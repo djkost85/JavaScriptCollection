7ab289c182f7966952ba7f9180b2adf1da36a60b
Mason Wan <masonaxcte@gmail.com>
2012-07-02 18:21:08 -0700

    Add release version. Rename debug version.

---
+++ PandoraAutoContinue/PandoraAutoContinue UserScripts.release.user.js
@@ -0,0 +1,27 @@
+// ==UserScript== http://wiki.greasespot.net/Metadata_block
+// @name            Pandora Auto Continue
+// @namespace       http://userscripts.org/users/masonaxcte
+// @description     Automatically click "I'm listening" and reload button in Pandora.com
+// @match           http://www.pandora.com/*
+// @run-at          document-end
+// @updateURL       https://github.com/masonaxcte/JavaScriptCollection/raw/master/PandoraAutoContinue/PandoraAutoContinue%20UserScripts.user.js
+// @version         1.2
+// ==/UserScript==
+
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function (mutations) {
+	mutations.forEach(function (mutation) {
+		for (var i = 0; i < mutation.addedNodes.length; i++) {
+			var addedNode = mutation.addedNodes[i];
+			var node = addedNode.parentNode.querySelector('a.toastItemReload, a.still_listening');
+
+			if (node) {
+				node.click();
+				return;
+			}
+		}
+	});
+});
+
+observer.observe(document, { childList: true, subtree: true });
