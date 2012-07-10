1b704649f8836e62ceb607df732595bbe750f0d7
Mason Wan <masonaxcte@gmail.com>
2012-07-10 08:12:04 -0700

    Update Demonoid and Kat.ph URL.

--- Download Search/Download Search.js
+++ Download Search/Download Search.js
@@ -1,18 +1,18 @@
 (function() {
 	var links = [
-		"http://beta.demonoid.me/files/?sort=S&query=%s",
-		"http://www.kat.ph/usearch/%s/",
+		"http://www.demonoid.ph/files/?query=%s",
+		"https://kat.ph/search/%s",
 		"https://www.google.com/cse?cx=014395573825771924245:6cxgcwinhte&q#gsc.tab&gsc.q=%s",
-		"https://www.google.com/search?q=mediafire|rapidshare|4shared|fileserve|hotfile|filesonic|depositfiles|zshare %s"
+		"https://www.google.com/search?q=FilesTube|4Shared|MediaFire|RapidShare|Box.net %s"
 	];
 
 	var query = window.getSelection().toString().trim();
 	query = prompt('Download Search', query);
 
 	if ((query != null) && (query = encodeURIComponent(query.trim()))) {
-		for (i = 0, len = links.length; i < len; i++) {
+		for (i = 0; i < links.length; i++) {
 			link = links[i];
 			window.open(link.replace('%s', query));
 		}
 	}
-}).call(this);
+})();
