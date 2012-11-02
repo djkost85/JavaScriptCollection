439c6bc3af912b3662411717260d0eef70dc72d2
Mason Wan <masonaxcte@gmail.com>
2012-11-02 06:21:01 -0700

    Add ThePirateBay and Torrentz.

--- Download Search/Download Search.js
+++ Download Search/Download Search.js
@@ -1,17 +1,19 @@
-(function() {
-	var links = [
-		"https://kat.ph/usearch/%s/",
+(function () {
+    var links = [
 		// "https://www.google.com/cse?cx=014395573825771924245:6cxgcwinhte&q#gsc.tab&gsc.q=%s",
-		"https://www.google.com/search?q=%s AND (4Shared OR MediaFire OR RapidShare OR Scribd OR DepositFiles OR Hotfile)"
-	];
+		// "https://www.google.com/search?q=%s AND (4Shared OR MediaFire OR RapidShare OR Scribd OR DepositFiles OR Hotfile)",
+        "http://thepiratebay.se/search/%s",
+        "http://torrentz.eu/search?f=%s",
+		"https://kat.ph/usearch/%s/"
+    ];
 
-	var query = window.getSelection().toString().trim();
-	query = prompt('Download Search', query);
+    var query = window.getSelection().toString().trim();
+    query = prompt('Download Search', query);
 
-	if ((query != null) && (query = encodeURIComponent(query.trim()))) {
-		for (var i = 0; i < links.length; i++) {
-			var link = links[i];
-			window.open(link.replace('%s', query));
-		}
-	}
+    if ((query != null) && (query = encodeURIComponent(query.trim()))) {
+        for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            window.open(link.replace('%s', query));
+        }
+    }
 })();
