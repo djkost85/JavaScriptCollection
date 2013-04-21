792d7959af16352aa5b5e05936e154fe7737c9bf
Mason Wan <masonwan@gmail.com>
2013-04-21 16:59:28 -0700

    Refactory the code, using better method to override the style

--- FavoriteStyle/favoriteStyle.js
+++ FavoriteStyle/favoriteStyle.js
@@ -1,20 +1,21 @@
 (function () {
+    var id = "C4365D9F-F7E8-4D17-84E4-E351FD1B640A";
+    var styleElement = null;
 
-	var id = "C4365D9F-F7E8-4D17-84E4-E351FD1B640A";
-	var style = "* {font-family: Calibri !important;} body {max-width: 61.80339887498948% !important; margin: 0 auto !important}";
-	var styleElement = null;
-	var elementId = "2F569819-CF80-44D3-87D7-406A02334818";
+    console.log('window[id]', window[id]);
 
-	if (window[id]) {
-		styleElement = document.getElementById(elementId);
-		document.body.removeChild(styleElement);
-	} else {
-		styleElement = document.createElement('style');
-		styleElement.id = elementId;
-		styleElement.innerHTML = style;
+    if (window[id]) {
+        if (document.getElementById(window[id].id)) {
+            document.body.removeChild(window[id]);
+        } else {
+            document.body.appendChild(window[id]);
+        }
+    } else {
+        styleElement = document.createElement('style');
+        styleElement.id = "2F569819-CF80-44D3-87D7-406A02334818";
+        styleElement.innerHTML = "* {font-family: Calibri !important; font-size: 150%;} body {max-width: 61.80339887498948% !important; margin: 0 auto !important; min-width: 600px !important;}";
 
-		document.body.appendChild(styleElement);
-	}
-
-	window[id] = !Boolean(window[id]);
+        window[id] = styleElement;
+        document.body.appendChild(styleElement);
+    }
 })();
