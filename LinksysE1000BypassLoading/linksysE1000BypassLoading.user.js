1eb283a1878f44de2f2e8eab2951505a3499e761
Mason Wan <masonwan@gmail.com>
2013-05-07 20:26:23 -0700

    Add auto-clicking for changing access policy

--- LinksysE1000BypassLoading/linksysE1000BypassLoading.user.js
+++ LinksysE1000BypassLoading/linksysE1000BypassLoading.user.js
@@ -5,29 +5,36 @@
 // @match           http://192.168.1.1/*
 // @run-at          document-end
 // @updateURL       
-// @version         1.0
+// @version         1.1
 // ==/UserScript==
 
 (function () {
 	var id = setInterval(onTimeout, 500);
+	console.log('id = ' + id);
 
 	function onTimeout() {
 		var iframeElement = document.querySelector('#loadstatus');
-
-		if (iframeElement == null) {
-			return;
-		};
-
-		var buttonElement = iframeElement.contentDocument.querySelector('.wpsbutton');
-
+		var buttonElement, buttonElements;
+		var i;
+		
+		buttonElement = document.querySelector('input[value="Continue"]');
+		
 		if (buttonElement != null) {
 			buttonElement.click();
-		};
+		}
 
 		buttonElement = document.querySelector('input[name=btaction]');
 
 		if (buttonElement != null) {
 			buttonElement.click();
+		};
+
+		if (iframeElement != null) {
+			buttonElement = iframeElement.contentDocument.querySelector('.wpsbutton');
+
+			if (buttonElement != null) {
+				buttonElement.click();
+			};
 		};
 	}
 })();
