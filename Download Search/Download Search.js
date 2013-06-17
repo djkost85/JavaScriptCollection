8661ca1193ec42c03fb77cd3d223b0fe8632863e
Mason Wan <masonwan@gmail.com>
2013-06-16 19:32:13 -0700

    Update URL of KickAssTorrent

--- Download Search/Download Search.js
+++ Download Search/Download Search.js
@@ -1,10 +1,10 @@
 (function () {
     var links = [
-		// "https://www.google.com/cse?cx=014395573825771924245:6cxgcwinhte&q#gsc.tab&gsc.q=%s",
-		// "https://www.google.com/search?q=%s AND (4Shared OR MediaFire OR RapidShare OR Scribd OR DepositFiles OR Hotfile)",
+        // "https://www.google.com/cse?cx=014395573825771924245:6cxgcwinhte&q#gsc.tab&gsc.q=%s",
+        // "https://www.google.com/search?q=%s AND (4Shared OR MediaFire OR RapidShare OR Scribd OR DepositFiles OR Hotfile)",
         "http://thepiratebay.se/search/%s",
         "http://torrentz.eu/search?f=%s",
-		"https://kat.ph/usearch/%s/"
+        "http://kickass.to/usearch/%s/"
     ];
 
     var query = window.getSelection().toString().trim();
