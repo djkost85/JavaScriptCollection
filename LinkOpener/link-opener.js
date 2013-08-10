7d87b6050ca6d5c84f583336118fa78d9e2e3e14
Mason Wan <masonwan@gmail.com>
2013-08-09 23:21:28 -0700

    Change Ctrl+RightClick to MiddleClick

--- LinkOpener/link-opener.js
+++ LinkOpener/link-opener.js
@@ -66,7 +66,7 @@
 
 	function rightClickOnElement(element) {
 		var rightClickEvent = document.createEvent('MouseEvents');
-		rightClickEvent.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, true, false, false, false, 0, null);
+		rightClickEvent.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 1, null);
 		element.dispatchEvent(rightClickEvent);
 	}
 
