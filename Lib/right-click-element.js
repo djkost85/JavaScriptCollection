11d25b03912d4312ece5550235881f2cc2337cfa
Mason Wan <masonwan@gmail.com>
2013-08-14 03:01:37 -0700

    Add script for simulating right click on certain item

---
+++ Lib/right-click-element.js
@@ -0,0 +1,5 @@
+function rightClickOnElement(element) {
+    var rightClickEvent = document.createEvent('MouseEvents');
+    rightClickEvent.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 1, null);
+    element.dispatchEvent(rightClickEvent);
+}
