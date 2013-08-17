c8ad5cc222224457f6464e78d68a8189c7779bf3
Mason Wan <masonwan@gmail.com>
2013-08-16 23:34:49 -0700

    Increase version number

--- GoogleSearchEnhancement/GoogleSearchEnhancement.user.js
+++ GoogleSearchEnhancement/GoogleSearchEnhancement.user.js
@@ -5,7 +5,7 @@
 // @match           https://www.google.com/*
 // @run-at          document-start
 // @updateURL
-// @version         1.0
+// @version         1.1
 // ==/UserScript==
 
 (function () {
