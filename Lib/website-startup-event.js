6e17ef9220611382b3413ae489dfaeacf708afd3
Mason Wan <masonwan@gmail.com>
2013-10-26 03:58:31 -0700

    Change text order

--- Lib/website-startup-event.js
+++ Lib/website-startup-event.js
@@ -1,12 +1,13 @@
-window.addEventListener("load", onLoad);
 document.addEventListener("DOMContentLoaded", onReady);
-
-function onLoad() {
-	console.log('Load');
-	window.removeEventListener("load", onLoad);
-}
 
 function onReady() {
 	console.log('Ready');
 	document.removeEventListener("DOMContentLoaded", onReady);
+}
+
+window.addEventListener("load", onLoad);
+
+function onLoad() {
+	console.log('Load');
+	window.removeEventListener("load", onLoad);
 }
